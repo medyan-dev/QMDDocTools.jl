c09895216fbc2460c04c507608dfb61dcef4d91b
nhz2 <nhz2@cornell.edu>
2022-08-02 22:22:34 -0400

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.28

---
+++ src/QuartoDocstrings.jl
@@ -0,0 +1,5 @@
+module QuartoDocstrings
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using QuartoDocstrings
+using Test
+
+@testset "QuartoDocstrings.jl" begin
+    # Write your tests here.
+end
